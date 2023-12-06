2806c9468a7fbb0708cd1ee7f42b0e1f226e621c
kaushalji-123 <divyanshu838283fb@gmail.com>
2023-12-06 23:36:38 +0530

    Added CustomJobCritariya

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import Navbar from '../src/Components/Navbar/Navbar'
 import Header from '../src/Components/Header/Header'
 import Search from '../src/Components/SearchBar/Searchbar'
 import Jobcard from '../src/Components/JobCard/Jobcard'
-import Jobsdata from './Jobstore'
 import { useEffect, useState } from "react";
-import { collection, query, orderBy, getDocs } from "firebase/firestore";
+import { collection, query,where, orderBy, getDocs } from "firebase/firestore";
 import {db} from './firebase.config'
 function App() {
 
   const [jobs, setJobs] = useState([]);
+// Fetching the data from Firebase  
 
   const fetchJobs = async() =>{
+
+    // tempJob Array for pushing here the Firebase Data in Array Form
+
     const tempJobs = []
     const JobsRef = query(collection(db,"Jobs"));
+
+    // orderBy used for arrange the data in Desending Order using 'PostedOn' Date 
+
     const q = query(JobsRef, orderBy("postedOn" ,"desc"));
     const req = await getDocs(q);
     req.forEach((job)=>{
@@ -21,10 +27,32 @@ function App() {
         id: job.id,
         postedOn:job.data().postedOn.toDate()
       });
-      setJobs(tempJobs);
     });
+    setJobs(tempJobs);
   }
   
+
+  const fetchJobsCustom = async(jobCritaria) =>{ 
+
+    // tempJob Array for pushing here the Firebase Data in Array Form
+
+    const tempJobs = []
+    const JobsRef = query(collection(db,"Jobs"));
+
+    // orderBy used for arrange the data in Desending Order using 'PostedOn' Date 
+
+    const q = query(JobsRef,  where("type","==","jobCritaria.type"),  where("title","==","jobCritaria.title"),  where("location","==","jobCritaria.location"),  where("experience","==","jobCritaria.experience"), orderBy("postedOn" ,"desc"));
+    const req = await getDocs(q);
+    req.forEach((job)=>{
+      tempJobs.push(
+        {...job.data(),
+        id: job.id,
+        postedOn:job.data().postedOn.toDate()
+      });
+    });
+    setJobs(tempJobs);
+  }
+
   useEffect(() => {
     fetchJobs()
   },[])
@@ -35,7 +63,7 @@ function App() {
       <h1 className='Headline bg-slate-500 text-3xl  '></h1>
       <Navbar/>
       <Header/>
-      <Search/>
+      <Search fetchJobsCustom ={fetchJobsCustom}/>
       {jobs.map((job)=>(
       <Jobcard key={job.id}{...job} />
       ))}
--- src/Components/SearchBar/Searchbar.jsx
+++ src/Components/SearchBar/Searchbar.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+const Searchbar = (props) => {
 
-const Searchbar = () => {
+    const [jobcritaria,setJobcritaria] = useState({
+        title:"",
+        location:"",
+        experience:"",
+        type:""
+    })
+    const handleChange = (e)=>{
+        setJobcritaria((prevStates) =>{
+            return{
+               ...prevStates,
+               [e.target.name]:e.target.value
+            }
+        })
+    }
+    console.log(jobcritaria);
+
+    const search = async()=>{
+        await props.fetchJobsCustom(jobcritaria);
+    }
+  
   return (
     <div className="flex gap-4 my-10 justify-center px-10 ">
-        <select className="w-64 py-3 pl-4 bg-zinc-300 font-semibold rounded-md">
+        <select onChange={handleChange} name="title" value={jobcritaria.title} className="w-64 py-3 pl-4 bg-zinc-300 font-semibold rounded-md">
             <option value="" selected disabled hidden>Job Role</option>
             <option value="ios Developer">ios Developer</option>
             <option value="Android Develope">Android Developer</option>
@@ -10,7 +31,7 @@ const Searchbar = () => {
             <option value="Frontend Developer">Frontend Developer</option>
             <option value="Backend Developer">Backend Developer</option> 
         </select> 
-        <select className="w-64 py-3 pl-4 bg-zinc-300 font-semibold rounded-md">
+        <select onChange={handleChange}  name="type" value={jobcritaria.type}  className="w-64 py-3 pl-4 bg-zinc-300 font-semibold rounded-md">
             <option value="" selected disabled hidden>Job Type</option>
             <option value="Full-Timerr">Full-Timer</option>
             <option value="Part-Time">Part-Time</option>
@@ -18,20 +39,20 @@ const Searchbar = () => {
             <option value="Contract">Contract</option>
 
         </select> 
-        <select className="w-64 py-3 pl-4 bg-zinc-300 font-semibold rounded-md">
+        <select onChange={handleChange}  name="location" value={jobcritaria.location}  className="w-64 py-3 pl-4 bg-zinc-300 font-semibold rounded-md">
             <option value="" selected disabled hidden>Location</option>
             <option value="In-Office">In-Office</option>
             <option value="Remote">Remote</option>
             <option value="Hybrid">Hybrid</option>
         </select> 
-        <select className="w-64 py-3 pl-4 bg-zinc-300 font-semibold rounded-md">
+        <select onChange={handleChange}  name="experience" value={jobcritaria.experience}  className="w-64 py-3 pl-4 bg-zinc-300 font-semibold rounded-md">
             <option value="" selected disabled hidden>Experience</option>
             <option value="Fresher">Fresher</option>
             <option value="Junior Level">Junior Level</option>
             <option value="Mid-Level">Mid-Level</option>
             <option value="Senior-Level">Senior-Level</option>
         </select> 
-        <button className="w-64 bg-blue-600 text-white font-bold py-3 rounded-md hover:to-blue-900 hover:translate-y-1 hover:scale-95">Search</button>
+        <button onClick={search} className="w-64 bg-blue-600 text-white font-bold py-3 rounded-md hover:to-blue-900 hover:translate-y-1 hover:scale-95">Search</button>
     </div>
   )
 }
